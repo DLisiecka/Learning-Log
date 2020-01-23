6ab3540c69a4a5f0e319163d1bbbcfafb59169fa
DLisiecka <dominika.anna.lisiecka@gmail.com>
2020-01-23 20:36:17 +0100

    Modify urls.py, views.py, topic.html, topics.html to view detailed side of each topic

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path(r'^topics/$', views.topics, name='topics'),
+    #Strona szczegółowa dotycząca pojedynczego tematu.
+    path(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
 ]
