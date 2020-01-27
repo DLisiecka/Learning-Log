fd35b69b0a36cf5dee5006694a62f9343eee079b
DLisiecka <dominika.anna.lisiecka@gmail.com>
2020-01-27 14:14:16 +0100

    Create a new form and url for it

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path(r'^topics/$', views.topics, name='topics'),
     #Strona szczegółowa dotycząca pojedynczego tematu.
     path(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+    #Strona przeznaczona do dodawania nowego tematu
+    path(r'^new_topic/$', views.new_topic, name='new_topic'),
 ]
