686d2b789e0bc78fb441c6e71dff071125aa7bac
DLisiecka <dominika.anna.lisiecka@gmail.com>
2020-01-27 15:01:45 +0100

    Add a new topic

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 from .models import Topic
+from .forms import TopicForm
 
 # Create your views here.
 def index(request):
@@ -18,3 +22,17 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """Dodaj nowy temat."""
+    if request.method != 'POST':
+        #Nie przekazano żadnych danych, należy utworzyć pusty formularz.
+        form = TopicForm()
+    else:
+        #Przekazano dane za pomocą żądania POST, należy je przetworzyć.
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('topics'))
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
