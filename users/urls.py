02490c6c000f760f31f2775200dea6ea6bc556a5
DLisiecka <dominika.anna.lisiecka@gmail.com>
2020-04-29 21:05:35 +0200

    Apply styles for app

--- users/urls.py
+++ users/urls.py
@@ -7,7 +7,11 @@ urlpatterns = [
     #Strona logowania
     path('login/', LoginView.as_view(template_name = 'users/login.html'), name='login'),
     #Strona wylogowania
-    path(r'^logout/$', views.logout_view, name='logout'),
+    #path(r'^logout/$', views.logout_view, name='logout'),
     #Strona rejestracji
-    path(r'^register/$', views.register, name='register'),
+    #path(r'^register/$', views.register, name='register'),
+    #Strona wylogowania
+    path('logout/', views.logout_view, name='logout'),
+    #Strona rejestracji
+    path('register/', views.register, name='register'),
 ]
